a886b980c98ce7632e3d369d81d66552e6648108
Guru Reoul <gurureoul@gmail.com>
2023-05-24 20:15:37 -0400

    refactor completed

---
+++ src/score/mod.rs
@@ -0,0 +1,18 @@
+pub mod resources;
+mod systems;
+
+use crate::score::resources::*;
+use crate::score::systems::*;
+use bevy::prelude::*;
+
+pub struct ScorePlugin;
+
+impl Plugin for ScorePlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<Score>()
+            .init_resource::<HighScores>()
+            .add_system(update_score)
+            .add_system(update_high_scores)
+            .add_system(high_scores_updated);
+    }
+}
