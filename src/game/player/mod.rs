d4f4970a470c60b217e31e57cd50a77b6c07b8fd
Guru Reoul <gurureoul@gmail.com>
2023-05-28 19:49:09 -0400

    more concise adding of systems in player

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -21,20 +21,13 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.configure_set(ConfinementSystemSet.after(MovementSystemSet))
             .add_system(spawn_player.in_schedule(OnEnter(AppState::Game)))
-            .add_system(
-                player_movement
-                    .in_set(MovementSystemSet)
-                    .in_set(OnUpdate(AppState::Game))
-                    .in_set(OnUpdate(SimulationState::Running)),
-            )
-            .add_system(
-                confine_player_movement
-                    .in_set(ConfinementSystemSet)
-                    .in_set(OnUpdate(AppState::Game))
-                    .in_set(OnUpdate(SimulationState::Running)),
-            )
             .add_systems(
-                (enemy_hit_player, player_hit_star)
+                (
+                    player_movement.in_set(MovementSystemSet),
+                    confine_player_movement.in_set(ConfinementSystemSet),
+                    enemy_hit_player,
+                    player_hit_star,
+                )
                     .in_set(OnUpdate(AppState::Game))
                     .in_set(OnUpdate(SimulationState::Running)),
             )
