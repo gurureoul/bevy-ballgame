12923859ea9657a2fe7c02047fe3f4ddda200b65
Guru Reoul <gurureoul@gmail.com>
2023-05-28 20:05:50 -0400

    added onenter/onexit system for pause sim

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -2,6 +2,14 @@ use bevy::prelude::*;
 
 use super::SimulationState;
 
+pub fn pause_simulation(mut next_sim_state: ResMut<NextState<SimulationState>>) {
+    next_sim_state.set(SimulationState::Paused);
+}
+
+pub fn resume_simulation(mut next_sim_state: ResMut<NextState<SimulationState>>) {
+    next_sim_state.set(SimulationState::Running);
+}
+
 pub fn toggle_simulation(
     mut next_sim_state: ResMut<NextState<SimulationState>>,
     keyboard_input: Res<Input<KeyCode>>,
